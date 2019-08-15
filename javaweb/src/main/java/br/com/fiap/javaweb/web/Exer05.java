39064fb012fe235201c3bcc68ec4ff4c4842aa22
Logon Aluno <logonaluno@local.com>
2019-08-15 11:45:51 -0300

    15/08

--- javaweb/src/main/java/br/com/fiap/javaweb/web/Exer05.java
+++ javaweb/src/main/java/br/com/fiap/javaweb/web/Exer05.java
@@ -1,11 +1,24 @@
 package br.com.fiap.javaweb.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/exer05")
 public class Exer05 extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html"); 
+		PrintWriter out = resp.getWriter(); 
+		
+		
+	}
 }
