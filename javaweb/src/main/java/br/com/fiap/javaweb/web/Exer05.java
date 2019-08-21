ee3381392edb056b345b5d99c115e6da5301aedc
Logon Aluno <logonaluno@local.com>
2019-08-21 10:00:09 -0300

    21/08

--- javaweb/src/main/java/br/com/fiap/javaweb/web/Exer05.java
+++ javaweb/src/main/java/br/com/fiap/javaweb/web/Exer05.java
@@ -17,8 +17,17 @@ public class Exer05 extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html"); 
+		String desc = req.getParameter("desc"); 
+		String quant = req.getParameter("quant");
 		PrintWriter out = resp.getWriter(); 
-		
-		
+		out.println("<html><body>"); 
+		out.println("<h1>Supermercado</h1>"); 
+		out.println("<table border=\"1\">");
+		out.println("<tr><td>Descrição</td><td>Quantidade no estoque</td></tr>"); 
+		out.println("<tr><td>" + desc +"</td>"+"<td>"+ quant +"</td></tr>");
+		out.println("</table>");
+		out.println("</body></html>");
+		out.flush();
+		out.close();
 	}
 }
